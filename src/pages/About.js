dc4585e0be18a2a98b85170f54abae6a6490aa2e
Cathy <cathymak52@gmail.com>
2021-06-06 15:56:21 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+        cupiditate ab eaque unde expedita quisquam optio. Placeat libero iure
+        tempore beatae ipsum fugit nostrum alias molestias culpa consequatur.
+        Nisi similique tempora saepe! Labore eius earum provident laboriosam
+        vitae voluptatibus deserunt nihil dignissimos, laudantium facere
+        incidunt, ullam dolorem, ratione beatae iste.
+      </p>
+    </section>
+  );
+};
+
+export default About;
